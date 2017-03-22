e1156e8c6017bbbf2c3af5e8050f5ba05142f996
V1A <V1A@D58-V1A-SWK>
2017-03-22 09:27:31 -0400

    some modification based on linux build

--- HW_Prog2/main.cpp
+++ HW_Prog2/main.cpp
@@ -12,10 +12,10 @@
 
 using namespace std;
 
-int crossoverSize = 16;
+int crossoverSize = 2;
 //Standard Matrix Multiplication
 void RunStandardMatrixMultiplication(const int& dimension, const vector<int>& matA, const vector<int>& matB, vector<int>& matC) {
-	int i=0, j=0, k=0, temp=0;
+	int i = 0, j = 0, k = 0, temp = 0;
 	for (k = 0; k<dimension; k++) {
 		for (i = 0; i<dimension; i++) {
 			temp = matA[i*dimension + k];
@@ -34,7 +34,7 @@ vector<int> RunStrassenMatrixMultiplication(const int& originalDimension, int& d
 	else {
 		int newDimension = dimension / 2;
 		level++;
-		int nPartitionedMatrixCoords = originalDimension / ((int)pow(2, level));
+		//int nPartitionedMatrixCoords = originalDimension / ((int)pow(2, level));
 
 		//Divide the matrices A and B
 		vector<int> A11(newDimension*newDimension); vector<int> A12(newDimension*newDimension);
@@ -81,7 +81,7 @@ vector<int> RunStrassenMatrixMultiplication(const int& originalDimension, int& d
 				B22[i*newDimension + j] = matB[(((i + newDimension)*dimension) + newDimension) + j];
 			}
 		}
-		/*vector<int> S1 = B12 - B22;
+		vector<int> S1 = B12 - B22;
 		vector<int> S2 = A11 + A12;
 		vector<int> S3 = A21 + A22;
 		vector<int> S4 = B21 - B11;
@@ -90,66 +90,66 @@ vector<int> RunStrassenMatrixMultiplication(const int& originalDimension, int& d
 		vector<int> S7 = A12 - A22;
 		vector<int> S8 = B21 + B22;
 		vector<int> S9 = A11 - A21;
-		vector<int> S10 = B11 + B12;*/
+		vector<int> S10 = B11 + B12;
 
-		vector<int> P1 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, (B12 - B22));
+		vector<int> P1 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, S1);
 
-		vector<int> P2 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A12), B22);
+		vector<int> P2 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S2, B22);
 
-		vector<int> P3 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A21 + A22), B11);
+		vector<int> P3 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S3, B11);
 
-		vector<int> P4 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, (B21 - B11));
+		vector<int> P4 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, S4);
 
-		vector<int> P5 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A22), (B11 + B22));
+		vector<int> P5 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S5, S6);
 
-		vector<int> P6 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A12 - A22), (B21 + B22));
+		vector<int> P6 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S7, S8);
 
-		vector<int> P7 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 - A21), (B11 + B12));
+		vector<int> P7 = RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S9, S10);
 
 		//C11=P5+P4-P2+P6
 		vector<int> tempC1(newDimension*newDimension);
 		tempC1 = P5 + P4 - P2 + P6;
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A22), (B11 + B22)) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, (B21 - B11)) -
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A12), B22) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A12 - A22), (B21 + B22));*/
-			//
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S5, 0, 0, S6, 0, 0) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS + nPartitionedMatrixCoords, colAS + nPartitionedMatrixCoords, S4, 0, 0) -
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S2, 0, 0, matB, rowBS + nPartitionedMatrixCoords, colBS + nPartitionedMatrixCoords) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S7, 0, 0, S8, 0, 0);*/
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A22), (B11 + B22)) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, (B21 - B11)) -
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A12), B22) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A12 - A22), (B21 + B22));*/
+		//
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S5, 0, 0, S6, 0, 0) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS + nPartitionedMatrixCoords, colAS + nPartitionedMatrixCoords, S4, 0, 0) -
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S2, 0, 0, matB, rowBS + nPartitionedMatrixCoords, colBS + nPartitionedMatrixCoords) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S7, 0, 0, S8, 0, 0);*/
 
 		//C12=P1+P2
 		vector<int> tempC2(newDimension*newDimension);
 		tempC2 = P1 + P2;
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, (B12 - B22)) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A12), B22);*/
-			//
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS, colAS, S1, 0, 0) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S2, 0, 0, matB, rowBS+nPartitionedMatrixCoords, colBS+ nPartitionedMatrixCoords);*/
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, (B12 - B22)) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A12), B22);*/
+		//
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS, colAS, S1, 0, 0) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S2, 0, 0, matB, rowBS+nPartitionedMatrixCoords, colBS+ nPartitionedMatrixCoords);*/
 
 		//C21=P3+P4
 		vector<int> tempC3(newDimension*newDimension);
 		tempC3 = P3 + P4;
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A21 + A22), B11) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, (B21 - B11));*/
-			//
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S3, 0, 0, matB, rowBS, colBS) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS+nPartitionedMatrixCoords, colAS+nPartitionedMatrixCoords, S4, 0, 0);*/
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A21 + A22), B11) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A22, (B21 - B11));*/
+		//
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S3, 0, 0, matB, rowBS, colBS) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS+nPartitionedMatrixCoords, colAS+nPartitionedMatrixCoords, S4, 0, 0);*/
 
 		//C22=P5+P1-P3-P7
 		vector<int> tempC4(newDimension*newDimension);
 		tempC4 = P5 + P1 - P3 - P7;
-			/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A22), (B11 + B22)) +
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, (B12 - B22)) -
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A21 + A22), B11) -
-			RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 - A21), (B11 + B12));*/
-			//
-			//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S5, 0, 0, S6, 0, 0) +
-			//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS, colAS, S1, 0, 0) -
-			//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S3, 0, 0, matB, rowBS, colBS) -
-			//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S9, 0, 0, S10, 0, 0);
-		
+		/*RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 + A22), (B11 + B22)) +
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, A11, (B12 - B22)) -
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A21 + A22), B11) -
+		RunStrassenMatrixMultiplication(originalDimension, newDimension, level, (A11 - A21), (B11 + B12));*/
+		//
+		//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S5, 0, 0, S6, 0, 0) +
+		//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, matA, rowAS, colAS, S1, 0, 0) -
+		//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S3, 0, 0, matB, rowBS, colBS) -
+		//RunStrassenMatrixMultiplication(originalDimension, newDimension, level, S9, 0, 0, S10, 0, 0);
+
 		for (int i = 0; i < newDimension; i++) {
 			for (int j = 0; j < newDimension; j++) {
 				matC[i*dimension + j] = tempC1[i*newDimension + j];
@@ -157,17 +157,17 @@ vector<int> RunStrassenMatrixMultiplication(const int& originalDimension, int& d
 		}
 		for (int i = 0; i < newDimension; i++) {
 			for (int j = newDimension; j < dimension; j++) {
-				matC[i*dimension + j] = tempC2[i*newDimension + (j-newDimension)];
+				matC[i*dimension + j] = tempC2[i*newDimension + (j - newDimension)];
 			}
 		}
 		for (int i = newDimension; i < dimension; i++) {
 			for (int j = 0; j < newDimension; j++) {
-				matC[i*dimension + j] = tempC3[(i-newDimension)*newDimension + j];
+				matC[i*dimension + j] = tempC3[(i - newDimension)*newDimension + j];
 			}
 		}
 		for (int i = newDimension; i < dimension; i++) {
 			for (int j = newDimension; j < dimension; j++) {
-				matC[i*dimension + j] = tempC4[(i - newDimension)*newDimension + (j-newDimension)];
+				matC[i*dimension + j] = tempC4[(i - newDimension)*newDimension + (j - newDimension)];
 			}
 		}
 	}
@@ -176,7 +176,7 @@ vector<int> RunStrassenMatrixMultiplication(const int& originalDimension, int& d
 
 void ProduceOutput(const vector<int>& matC, const int& dimension) {
 	for (int i = 0; i < dimension; i++) {
-		cout << matC[i*dimension + i] <<endl;
+		cout << matC[i*dimension + i] << endl;
 	}
 }
 
@@ -205,7 +205,7 @@ int main(int argc, char** argv) {
 	int dimension;
 	if (!(ss1 >> dimension))
 		cerr << "Invalid number for dimension" << argv[2] << '\n';
-	
+
 	vector<int> matrixA(dimension*dimension);
 	vector<int> matrixB(dimension*dimension);
 	//vector<int> matrixC(dimension*dimension);
@@ -233,11 +233,11 @@ int main(int argc, char** argv) {
 
 	//Strassen Matrix Multiplication
 	tStart = steady_clock::now();
-	vector<int> returnMatrixC = RunStrassenMatrixMultiplication(dimension,dimension,0,matrixA,matrixB);
+	vector<int> returnMatrixC = RunStrassenMatrixMultiplication(dimension, dimension, 0, matrixA, matrixB);
 	tEnd = steady_clock::now();
 	//diff = tEnd - tStart;
-	cout << "Time for STRASSEN multiplication with dimension: " << dimension << " crossover: " <<crossoverSize
-		<< " : " << chrono::duration_cast<chrono::seconds>(tEnd - tStart).count() << " seconds" << endl;
+	cout << "Time for STRASSEN multiplication with dimension: " << dimension << " crossover: " << crossoverSize
+		<< " : " << chrono::duration_cast<chrono::milliseconds>(tEnd - tStart).count() << " seconds" << endl;
 
 	//Output
 	//ProduceOutput(returnMatrixC, dimension);
